7d8f35ab523f8aa9815e23ff5f46b69a0830397a
Xazu <Xazu001@users.noreply.github.com>
2023-05-15 19:09:54 +0200

    Main1

---
+++ app.js
@@ -0,0 +1,79 @@
+// tiles
+
+const wrapper = document.querySelectorAll(".whyme-tiles");
+
+let columns = 0,
+    rows = 0;
+
+const getToggled = (grid) => {
+    return grid.getAttribute("data-toggled") == "true" ?? false;
+};
+
+const toggle = (grid) => {
+    const toggled = getToggled(grid);
+
+    grid.setAttribute("data-toggled", !toggled);
+
+    console.log(wrapper);
+
+    wrapper.forEach(i => i.classList.toggle("toggled"));
+    // wrapper.forEach(i => i.parentElement.querySelector(".whyme-text").classList.toggle("hide"));
+};
+
+const handleOnClickTiles = (tile, index) => {
+    // console.log(tile, tile.parentElement);
+
+    const grid = tile.parentElement;
+
+    toggle(grid);
+    // grid.querySelectorAll(".whyme-text").forEach(i => i.classList.add("hide"));
+    tile.parentElement.parentElement.children[2].classList.toggle("hide");
+    tile.parentElement.parentElement.children[0].classList.toggle("hide");
+
+    anime({
+        targets: grid.children,
+        opacity: getToggled(grid) ? 0 : 1,
+        delay: anime.stagger(60, {
+            grid: [columns, rows],
+            from: index,
+        }),
+    });
+};
+
+const createTile = (index) => {
+    const tile = document.createElement("div");
+
+    tile.classList.add("tile");
+
+    tile.style.opacity = 1;
+
+    tile.onclick = () => handleOnClickTiles(tile, index);
+
+    return tile;
+};
+
+const createTiles = (quantity) => {
+    for (let i = 0; i < quantity; i++) {
+        wrapper.forEach(e => e.appendChild(createTile(i)));
+    };
+};
+
+const createGrid = () => {
+    wrapper.innerHTML = "";
+
+    const size = wrapper.clientWidth > 1000 ? 100 : 25;
+
+    columns = Array.from(document.querySelectorAll(".whyme-item")).map(i => Math.floor(i.clientWidth / size + 3))[0];
+    rows = Array.from(document.querySelectorAll(".whyme-item")).map(i => Math.floor(i.clientHeight / size + 3))[0];
+
+    wrapper.forEach(e => e.style.setProperty("--columns", columns));
+    wrapper.forEach(e => e.style.setProperty("--rows", rows));
+
+    console.log(columns, rows);
+
+    createTiles(columns * rows);
+};
+
+createGrid();
+
+window.onresize = () => createGrid();
